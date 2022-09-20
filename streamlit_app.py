635a1ef9342cc8a519e8cd9b0daa59522adc1147
aelikapa <114008528+aelikapa@users.noreply.github.com>
2022-09-20 21:12:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('MY PARENTS HAVE A HEALTHY DINER')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Eggs')
+streamlit.text('Oatmeal')
+streamlit.text('Spinach')
